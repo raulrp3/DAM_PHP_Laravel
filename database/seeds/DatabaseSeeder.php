38a5ce600b99faeb8cf35e4e20665ab2361cf396
raulrp3 <raul.ramirez@escuelaestech.es>
2020-01-20 19:17:25 +0100

    Creando profesiones y usuarios de forma manual mediante seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->truncateTables([
+            'professions',
+            'users'
+        ]);
+
+        $this->call(ProfessionSeeder::class);
+        $this->call(UserSeeder::class);
+    }
+
+    protected function truncateTables(array $tables){
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+        foreach($tables as $table){
+            DB::table($table)->truncate();
+        } 
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
     }
 }
