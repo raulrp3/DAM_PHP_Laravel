4fb48aeac2c81c1aee26e7341441ab18b8d9a5de
raulrp3 <raul.ramirez@escuelaestech.es>
2020-01-20 18:53:58 +0100

    Creada relacion entre las tablas users y professions

--- database/migrations/2020_01_20_172559_create_professions_table.php
+++ database/migrations/2020_01_20_172559_create_professions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddProfessionToUsers extends Migration
+class CreateProfessionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class AddProfessionToUsers extends Migration
      */
     public function up()
     {
-        Schema::table('users', function(Blueprint $table){
-            $table->string('profession', 50)->nullable()->after('password');
+        Schema::create('professions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 100);
+            $table->timestamps();
         });
     }
 
@@ -25,8 +27,6 @@ class AddProfessionToUsers extends Migration
      */
     public function down()
     {
-        Schema::table('users', function(Blueprint $table){
-            $table-dropColumn('profession');
-        });
+        Schema::dropIfExists('professions');
     }
 }
