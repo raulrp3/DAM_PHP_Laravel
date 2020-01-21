2cd51aa28ef2d89114766d44b18bd39e2eaf0e3f
raulrp3 <raul.ramirez@escuelaestech.es>
2020-01-21 16:35:33 +0100

    Creada vista 404.blade.php

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('content')
+    <div class="container mt-5">
+        <div class="alert alert-danger text-center">
+            <h1>¡PÁGINA NO ENCONTRADA!</h1>
+            <p class="mt-4">¿Estás seguro que buscaba esta página?</p>
+        </div>
+    </div>
+@endsection
