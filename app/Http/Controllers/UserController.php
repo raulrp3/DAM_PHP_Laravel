77b95e580ee2593ec2da77c85e6aed2bec31a8bf
raulrp3 <raul.ramirez@escuelaestech.es>
2020-01-22 16:40:48 +0100

    Validando los datos del formulario para crear usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -34,7 +34,21 @@ class UserController extends Controller
     }
 
     public function store(){
-        $data = request()->all();
+        $data = request()->validate([
+            'name' => ['required', 'regex:/^[\pL\s\-]+$/u', 'min:2'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'min:6']
+        ], [
+            'name.required' => 'El campo nombre es obligatorio.',
+            'name.regex' => 'El campo nombre no es válido.',
+            'name.min' => 'El campo  nombre debe tener más de 2 caracteres.',
+            'email.required' => 'El campo correo electrónico es obligatorio.',
+            'email.unique' => 'El campo correo electrónico ya pertenece a otro usuario.',
+            'email.email' => 'El campo correo electrónico no es válido.',
+            'password.required' => 'El campo contraseña es obligatorio.',
+            'password.min' => 'El campo contraseña debe tener más de 6 caracteres.'
+        ]);
+
 
         User::create([
             'name' => $data['name'],
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,15 +8,24 @@
 
             <div class="form-group">
                 <label for="inputName">Nombre:</label>
-                <input type="text" class="form-control" id="inputName">
+                <input type="text" class="form-control" id="inputName" name="name" value="{{ old('name') }}">
+                @if($errors->has('name'))
+                    <div class="alert alert-danger mt-2">{{ $errors->first('name') }}</div>
+                @endif   
             </div>
             <div class="form-group">
                 <label for="inputEmail">Correo elctrónico:</label>
-                <input type="text" class="form-control" id="inputEmail">
+                <input type="email" class="form-control" id="inputEmail" name="email" value="{{ old('email') }}">
+                @if($errors->has('email'))
+                    <div class="alert alert-danger mt-2">{{ $errors->first('email') }}</div>
+                @endif 
             </div>
             <div class="form-group">
                 <label for="inputPass">Contraseña:</label>
-                <input type="password" class="form-control" id="inputPass">
+                <input type="password" class="form-control" id="inputPass" name="password">
+                @if($errors->has('password'))
+                    <div class="alert alert-danger mt-2">{{ $errors->first('password') }}</div>
+                @endif 
             </div>
             <input type="submit" class="btn btn-success mt-3" value="Crear usuario">
         </form>
