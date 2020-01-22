8c5e7d1317cb8f5aad926d5729a07323f5556033
raulrp3 <raul.ramirez@escuelaestech.es>
2020-01-22 16:43:15 +0100

    Mostrando el tipo de usuario en el detalle

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,6 +6,11 @@
         <h1 class="mb-3">Usuario #{{ $user->id }}.</h1>
         <p>Nombre: {{ $user->name }}.</p>
         <p>Correo electrónico: {{ $user->email }}.</p>
+        @if($user->is_admin == 1)
+            <p>Tipo de usuario: Administrador.</p>
+        @else
+            <p>Tipo de usuario: Usuario</p>
+        @endif
         <div class="alert alert-secondary mt-5">
             <a href="{{ route('users') }}" class="btn btn-primary"><i class="fas fa-arrow-left"></i> Volver al listado de usuarios</a>
         </div>
