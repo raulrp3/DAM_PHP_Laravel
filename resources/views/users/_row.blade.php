735c954ba57b3e8152b1a5b81c362274579e48e9
raulrp3 <raul.ramirez@escuelaestech.es>
2020-02-10 21:13:44 +0100

    Mejorado el estilo de los botones de acciones

--- resources/views/users/_row.blade.php
+++ resources/views/users/_row.blade.php
@@ -2,7 +2,7 @@
     <td>{{ $user->id }}</td>
     <td>
         <div>
-            <p>{{ $user->name }}.</p>
+            <p><strong>{{ $user->name }}.</strong></p>
             <p>{{ $user->profile->profession->title }}</p>
         </div>
     </td>
@@ -21,28 +21,28 @@
     <td>
         <div>
             @if($user->trashed())
-                <div class="mt-2">
+                <div class="mt-2 d-flex justify-content-around">
                     <form action="{{ route('users.destroy', $user) }}" method="post" class="mt-2">
                         {{ csrf_field() }}
                         {{ method_field('DELETE') }}
 
-                        <button type="submit" class="btn btn-danger">Eliminar</button>
+                        <button type="submit" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
                     </form>
                     <form action="{{ route('users.restore', $user) }}" method="post" class="mt-2">
                         {{ csrf_field() }}
 
-                        <button type="submit" class="btn btn-info">Restaurar</button>
+                        <button type="submit" class="btn btn-info"><i class="fas fa-trash-restore"></i></button>
                     </form>
                 </div>
             @else
                 <div class="mt-2">
-                    <a href="{{ route('users.show', $user) }}" class="btn btn-info"><i class="fas fa-eye"></i> Ver detalles</a>
-                    <a href="{{ route('users.edit', $user) }}" class="btn btn-dark"><i class="fas fa-pen"></i> Modificar</a>
+                    <a href="{{ route('users.show', $user) }}" class="btn btn-info"><i class="fas fa-eye"></i></a>
+                    <a href="{{ route('users.edit', $user) }}" class="btn btn-dark"><i class="fas fa-pen"></i></a>
                     <form action="{{ route('users.trash', $user) }}" method="post" class="mt-2">
                         {{ csrf_field() }}
                         {{ method_field('PATCH') }}
 
-                        <button type="submit" class="btn btn-danger"><i class="fas fa-trash-alt"></i> Eliminar</button>
+                        <button type="submit" class="btn btn-danger"><i class="fas fa-user-times"></i></button>
                     </form>
                 </div>
             @endif
